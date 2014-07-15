5027f2a34e392cc04af990d300b4cdf52b4e984d
Marian Graurov <marian_graurov@abv.bg>
2014-07-15 17:39:58 +0300

    Exercises 3.01 - 3.02

---
+++ 3.01 ExtentionMethodSubstring/3.01 ExtentionMethodSubstring/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._01_ExtentionMethodSubstring
+{
+    static class SubstringExtention
+    {
+        public static StringBuilder Substring(this StringBuilder list, int index, int length)
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append(list.ToString().Substring(index, length));
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder list = new StringBuilder();
+            StringBuilder newList = new StringBuilder();
+            list.Append("abcdefghijklmnopqrstuvwxyz");
+            newList = list.Substring(1, 5);
+            Console.WriteLine(newList.ToString());
+        }
+    }
+
+    
+        
+    
+}
---
+++ 3.02 IEnumerableExtentions/3.02 IEnumerableExtentions/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._02_IEnumerableExtentions
+{
+    public static class IEnumerableExtentions<T> where T : IComparable<T>
+    {
+        public static T Sum<T>(this IEnumerable<T> elements)
+        {
+            dynamic result = 0;
+            foreach (var element in elements)
+            {
+                result += (dynamic)element;
+            }
+
+            return result;
+        }
+
+        public static T Product<T>(this IEnumerable<T> elements)
+        {
+            dynamic result = 1;
+            foreach (var element in elements)
+            {
+                result *= (dynamic)element;
+            }
+
+            return result;
+        }
+
+        public static T Min<T>(this IEnumerable<T> elements) where T : IComparable
+        {
+            dynamic result = ulong.MaxValue;
+            foreach (var element in elements)
+            {
+                if (result >= element)
+                {
+                    result = (dynamic)element;
+                }
+            }
+
+            return result;
+        }
+
+        public static T Max<T>(this IEnumerable<T> elements) where T : IComparable
+        {
+            dynamic result = ulong.MaxValue;
+            foreach (var element in elements)
+            {
+                if (result <= element)
+                {
+                    result = (dynamic)element;
+                }
+            }
+            return result;
+        }
+
+        public static T Average<T>(this IEnumerable<T> elements)
+        {
+            dynamic average = 0, counter = 0;
+            foreach (var c in elements)
+            {
+                average += c;
+                counter++;
+            }
+            if (counter == 0)
+                throw new ArgumentException("The passed collection is empty.");
+            return average / counter;
+        }
+    
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> result = from value in Enumerable.Range(0, 3)
+				  select value;
+
+            int a = result.Sum<int>();
+            
+        }
+    }
+}
